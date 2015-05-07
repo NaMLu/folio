7a5f287a818aa48bf3091e071c25f8867f917eab
peter <binmonk@gmail.com>
2015-05-07 16:03:46 +0300

    things , many things

--- resources/views/folio/index.blade.php
+++ resources/views/folio/index.blade.php
@@ -22,22 +22,25 @@
                     <div class="panel">
                         <div class="panel-heading">
                             <div class="panel-title"><h4>{{$item->name}}</h4></div>
-                        </div><!--.panel-heading-->
+                        </div>
+                        <!--.panel-heading-->
                         <div class="panel-body without-padding">
                             <div class="list-gallery">
                                 <div class="card tile card-black card-image bg-image sample-bg-image8 margin-0">
-                                </div><!--.card-->
-                            </div><!--.row-->
-                        </div><!--.panel-body-->
-                        <div class="panel-footer footer-dark footer-translucent">
-                            <ul class="justified-list">
-                                <li><i class="ion-android-favorite-outline"></i></li>
-                                <li><i class="ion-android-radio-button-off"></i></li>
-                                <li><i class="ion-android-checkbox-outline-blank"></i></li>
-                            </ul>
-                            
-                        </div><!--.panel-footer-->
-                    </div><!--.panel-->
+                                </div>
+                                <!--.card-->
+                            </div>
+                            <!--.row-->
+                        </div>
+                        <!--.panel-body-->
+                        <div class="panel-footer footer-dark footer-translucent" style="width: 100%; min-height: 20px;">
+                            @foreach($item->categories as $cat)
+                                <span class="label label-primary">{{$cat->name}}</span>
+                            @endforeach
+                        </div>
+                        <!--.panel-footer-->
+                    </div>
+                    <!--.panel-->
                 </div>
             @endforeach
         </div>
